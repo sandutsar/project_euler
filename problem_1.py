6790237344b5828b2cbc8c968b97265260555747
Roman Sandutsa <sandutsar@gmail.com>
2023-04-23 21:29:57 +0300

    Added custom limit support to problem_1

--- problem_1.py
+++ problem_1.py
@@ -4,11 +4,11 @@ import sys
 def problem_1(a=3, b=5, limit=int(1e3)):
     result = sum([num if num % a == 0 or num % b == 0 \
                   else 0 for num in range(limit)])
-    return sum(result)
+    return result
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 3:
+    if len(sys.argv) in [3, 4]:
         print(problem_1(*map(int, sys.argv[1:])))
     else:
         print(problem_1())
