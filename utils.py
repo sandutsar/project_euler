889d1976ddf0453cfd97bc3e64babfd3dd89f46d
Roman Sandutsa <sandutsar@gmail.com>
2023-04-24 17:25:45 +0300

    Added is_prime function

--- utils.py
+++ utils.py
@@ -3,3 +3,10 @@ def is_palindrome(n):
         return str(n) == str(n)[-1::-1]
     else:
         raise TypeError(f'Error: n = {n} must be int!')
+    
+
+def is_prime(n):
+    if isinstance(n, int):
+        return sum([n % div == 0 for div in range(1, n + 1)]) == 2
+    else:
+        raise TypeError(f'Error: n = {n} must be int!')
